a3e9baf4a8b3e06184271caf1f22cd99ded18c0c
Erick-Nascimento <55209050+Erick-Nascimento@users.noreply.github.com>
2025-05-07 11:47:38 -0300

    fix: change base api url

--- services/users.ts
+++ services/users.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://api.brasas.com"; //"http://localhost:8000";
 
 export const makeLogin = async (user_id, community_id) => {
   const response = await fetch(`${API_BASE_URL}/users/login/${user_id}`, {
