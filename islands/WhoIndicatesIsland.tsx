a63d002d3dc285cb8fc73e336d6aa9a45496cf58
Erick-Nascimento <55209050+Erick-Nascimento@users.noreply.github.com>
2025-10-03 08:37:56 -0300

    fix: add more logs

--- islands/WhoIndicatesIsland.tsx
+++ islands/WhoIndicatesIsland.tsx
@@ -49,8 +49,11 @@ export default function WhoIndicatesIsland(props) {
           const userId = data.userId;
 
           const access_token = await makeLogin(userId, community_id);
+          console.log("access_token:", access_token);
           const userEmail = await getUserEmail(userId, access_token);
+          console.log("userEmail:", userEmail);
           const externalId = await getExternalId(userEmail, access_token);
+          console.log("externalId:", externalId);
           setFinalExternalId(externalId);
         } catch (error) {
           console.error("Erro ao obter dados do usuário:", error);
