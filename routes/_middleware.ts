e7f14cde98351e689a188bc24fafc78ffbbd72c6
Erick-Nascimento <55209050+Erick-Nascimento@users.noreply.github.com>
2025-12-15 11:37:07 -0300

    feat: change url to back to hub button and change community id

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -5,7 +5,7 @@ export async function handler(req: Request, ctx: FreshContext) {
 
   resp.headers.set(
     "Content-Security-Policy",
-    "frame-ancestors 'self' *.layers.education paneltest.brasas.com",
+    "frame-ancestors 'self' *.layers.education paneltest.brasas.com hub.brasas.com",
   );
 
   return resp;
